d78682c2405fbaf70f84ffdac7f64d9437ac4184
t_hayashi <t.hayashi0077+github@gmail.com>
2016-11-14 21:35:53 +0900

    Initial Commit

---
+++ ExceptionToSwift/ExceptionToSwift.h
@@ -0,0 +1,19 @@
+//
+//  ExceptionToSwift.h
+//  ExceptionToSwift
+//
+//  Created by 林達也 on 2016/11/14.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExceptionToSwift.
+FOUNDATION_EXPORT double ExceptionToSwiftVersionNumber;
+
+//! Project version string for ExceptionToSwift.
+FOUNDATION_EXPORT const unsigned char ExceptionToSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExceptionToSwift/PublicHeader.h>
+
+
